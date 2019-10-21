ef62185484ce559c8db383c70719e9d2a949d9f5
Amorphis81 <petikantrop@gmail.com>
2019-10-22 00:04:02 +0300

    day 2

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -4,4 +4,17 @@ $(document).ready(function () {
     $('.sandwich').on('click', function(e) {
        $('.sandwich-menu').toggleClass('right-0');
     });
+
+    $('.header-menu__item').on('click', function(e){
+        var items = $('.header-menu__item');
+        var currentItem = $(this);
+        for (var i = 0; i < items.length; i++){
+            if ( items[i] != currentItem ) {
+                console.log(items[i]);
+                // items[i].removeClass('active');
+            }
+        }
+        // $('.header-menu__item').removeClass('active');
+        $(this).toggleClass('active');
+    })
 });
